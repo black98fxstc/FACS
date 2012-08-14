1043a6fffaa259c85453f75c9c002b6a195c076f
wmoore <wmoore>
2012-08-14 04:42:19 +0000

    get scatter parameter names from list instead of hard wired

--- CompensationDev/src/edu/stanford/facs/compensation/ControlData.java
+++ CompensationDev/src/edu/stanford/facs/compensation/ControlData.java
@@ -25,25 +25,22 @@ public abstract class ControlData
     throws FCSException, IOException
   {
     float[][] data = super.read();
-    FCSParameter p = fcs.getParameter("FSC-A");
-    assert p != null : "No FCS parameter FSC-A";
-    float[] fsc = data[p.getIndex() - 1];
-
-    p = fcs.getParameter("SSC-A");
-    assert p != null : "No FCS parameter SSC-A";
-    float[] ssc = data[p.getIndex() - 1];
 
     String[] detectorList = comp.getDetectorList();
     X = new float[detectorList.length][];
     for (int j = 0; j <  detectorList.length; ++j)
     {
-      p = fcs.getParameter(detectorList[j]);
+      FCSParameter p = fcs.getParameter(detectorList[j]);
       assert p != null : "No FCS parameter " + detectorList[j];
       X[j] = data[p.getIndex() - 1];
     }
-
-    Y = new float[2][];
-    Y[0] = fsc;
-    Y[1] = ssc;
+    
+    Y = new float[comp.scatter.length][];
+    for (int j = 0; j < comp.scatter.length; ++j)
+    {
+    	FCSParameter p = fcs.getParameter(comp.scatter[j]);
+      assert p != null : "No FCS parameter " + comp.scatter[j];
+      Y[j] = data[p.getIndex() - 1];
+    }
   }
 }
